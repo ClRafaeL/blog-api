c17d3438555cbedd606cecec5a980c84e6119bed
Rafael Claudio <c.rafaelneto7@gmail.com>
2019-03-24 20:43:11 -0300

    add o arquivos messages.py

---
+++ apps/messages.py
@@ -0,0 +1,7 @@
+MSG_INVALID_DATA = 'Ocorreu um erro nos campos informados.'
+MSG_DOES_NOT_EXIST = 'Este(a) {} não existe.'
+MSG_EXCEPTION = 'Ocorreu um erro no servidor. Contate o administrador.'
+MSG_ALREADY_EXISTS = 'Já existe um(a) {} com estes dados.'
+MSG_RESOURCE_CREATED = '{} criado.'
+MSG_FIELD_REQUIRED = 'Campo necessario.'
+MSG_NO_DATA = 'Corpo da msg esta Vazio.'

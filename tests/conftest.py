6c307f03222aea7e7dd1f412c6e9162f81e29c13
Rafael Claudio <c.rafaelneto7@gmail.com>
2019-03-27 19:19:41 -0300

    ajuste codigo

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 _ENV_FILE = join(dirname(__file__), '../.env')
 
 if isfile(_ENV_FILE):
-    load_dotenv(dotenv_path=_ENV_FILE)
+    load_dotenv(dotenv_path = _ENV_FILE)
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope = 'session')
 def client():
     from apps import create_app
 
@@ -22,7 +22,7 @@ def client():
 
     ctx.pop()
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope = 'function')
 def mongo(request, client):
     
     def fin():
